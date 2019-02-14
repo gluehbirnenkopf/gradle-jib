e9541c1d6244167002f5384a6af56580dae61328
gluehbirnenkopf <mail@leongregori.com>
2019-02-14 11:28:42 +0100

    added jenkins dsl

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,13 +2,17 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloController {
 
+    @Value("${greeting.text}")
+    private String someOutput;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return someOutput;
     }
 
 }
